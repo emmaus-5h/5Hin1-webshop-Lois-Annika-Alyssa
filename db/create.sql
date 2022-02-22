3fcb83f64d18e19d37c9e06a1bf906a06036da52
AnnikaMG <74548149+AnnikaMG@users.noreply.github.com>
2022-02-22 10:27:42 +0000

    tabellen linken

--- db/create.sql
+++ db/create.sql
@@ -8,7 +8,8 @@ CREATE TABLE products (
   name VARCHAR(255),
   description TEXT,
   symboliek TEXT,
-  price NUMERIC(10, 2)
+  price NUMERIC(10, 2),
+  verzorging NUMERIC(10, 2)
 );
 
 
@@ -21,12 +22,12 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, symboliek, price) values ('Original', 'Een van onze meest populaire boeketten de bloemen zijn op verschillende hoogtes afgesneden zodat de bloemen de ruimte krijgen. Perfect als centerpiece op tafel', '816905633-0', 'werkt dit', 25.0);
-insert into products (name, description, code, symboliek, price) values ('Deluxe', 'Onze deluxe bos is een echte showstopper. Door de lange stelen is het boeket perfect geschikt voor een strak interieur of op de eettafel.', '077030122-3', 'text', 35);
-insert into products (name, description, code, symboliek, price) values ('Pretty in Pink', 'Een rijk gevulde bos met fluweelzachte roze bloemen. Door de compacte en ronde vorm is de bos perfect om te schikken.', '445924201-X', 'text', 32.5);
-insert into products (name, description, code, symboliek, price) values ('Pixie', 'Compact in formaat dit is onze prachtige bos Pixie. De bos heeft een stijlvolle look. De roze tinten in de bos zorgen ervoor dat dit de perfecte eyecatcher voor op de eettafel is.', '693155505-7', 'text', 25);
-insert into products (name, description, code, symboliek, price) values ('Darling', 'Darling is een rijkgevulde bos met voornamelijk poeder roze tinten. In deze bos zitten onder andere prachtige rozen en chrysanten.', '686928463-6', 'text', 24.95);
-insert into products (name, description, code, symboliek, price) values ('Atelier', 'Dit boeket is gevuld met luxe bloemen met lange stelen. Het boeket bevat vooral warme tinten en ook onder andere een prachtige Japanse bloesemtak', '492662523-7', 'text', 30.95);
+insert into products (name, description, code, symboliek, price, verzorging) values ('Original', 'Een van onze meest populaire boeketten de bloemen zijn op verschillende hoogtes afgesneden zodat de bloemen de ruimte krijgen. Perfect als centerpiece op tafel', '816905633-0', 'werkt dit', 25.0, 1);
+insert into products (name, description, code, symboliek, price, verzorging) values ('Deluxe', 'Onze deluxe bos is een echte showstopper. Door de lange stelen is het boeket perfect geschikt voor een strak interieur of op de eettafel.', '077030122-3', 'text', 35, 3);
+insert into products (name, description, code, symboliek, price, verzorging) values ('Pretty in Pink', 'Een rijk gevulde bos met fluweelzachte roze bloemen. Door de compacte en ronde vorm is de bos perfect om te schikken.', '445924201-X', 'text', 32.5, 2);
+insert into products (name, description, code, symboliek, price, verzorging) values ('Pixie', 'Compact in formaat dit is onze prachtige bos Pixie. De bos heeft een stijlvolle look. De roze tinten in de bos zorgen ervoor dat dit de perfecte eyecatcher voor op de eettafel is.', '693155505-7', 'text', 25, 4);
+insert into products (name, description, code, symboliek, price, verzorging) values ('Darling', 'Darling is een rijkgevulde bos met voornamelijk poeder roze tinten. In deze bos zitten onder andere prachtige rozen en chrysanten.', '686928463-6', 'text', 24.95, 1);
+insert into products (name, description, code, symboliek, price, verzorging) values ('Atelier', 'Dit boeket is gevuld met luxe bloemen met lange stelen. Het boeket bevat vooral warme tinten en ook onder andere een prachtige Japanse bloesemtak', '492662523-7', 'text', 30.95, 4);
 
 
 CREATE TABLE verzorging (
