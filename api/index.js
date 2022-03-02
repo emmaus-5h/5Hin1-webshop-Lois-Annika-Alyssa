2222e3546fe95fe4be18bd3f3a350b35716af948
AnnikaMG <74548149+AnnikaMG@users.noreply.github.com>
2022-03-02 21:24:19 +0000

    api aangepast

--- api/index.js
+++ api/index.js
@@ -68,28 +68,19 @@ function getCategories(request, response) {
 function getProducts(request, response) {
   console.log('API ontvangt /api/products/', request.query)
   let data = []
-  const sqlOpdracht = db.prepare('SELECT products.id AS id, products.name AS name, products.description AS description, products.symboliek AS symboliek, products.code AS code, products.price AS price FROM products ORDER BY products.name ASC')
+  const sqlOpdracht = db.prepare('SELECT products.id AS id, products.name AS name, products.description AS description, products.symboliek AS symboliek, products.verzorging AS verzorging, products.code AS code, products.price AS price FROM products ORDER BY products.name ASC')
   data = sqlOpdracht.all()
   // console.log(JSON.stringify(data, null, 2))
   response.status(200).send(data)
   console.log('API verstuurt /api/products/')
 }
 
-function getProduct(request, response) {
-  console.log('API ontvangt /api/products/', request.query)
-  let data = []
-  const sqlOpdracht = db.prepare('SELECT verzorging.klimaat AS klimaat, verzorging.specification AS specification, verzorging.voeding AS voeding  FROM verzorging ORDER BY verzorging.klimaat ASC')
-  data = sqlOpdracht.all()
-  // console.log(JSON.stringify(data, null, 2))
-  response.status(200).send(data)
-  console.log('API verstuurt /api/products/')
-}
 
 function getProductById(request, response) {
   console.log('API ontvangt /api/products/:id', request.query)
   let data = []
   const product_id = parseInt(request.params.id)
-  const sqlOpdracht = db.prepare('SELECT products.id AS id, products.name AS name, products.description AS description, products.symboliek AS symboliek, products.code AS code, products.price AS price FROM products WHERE id = ?')
+  const sqlOpdracht = db.prepare('SELECT products.id AS id, products.name AS name, products.description AS description, products.symboliek AS symboliek, products.verzorging AS verzorging, products.code AS code, products.price AS price FROM products WHERE id = ?')
   data = sqlOpdracht.all(product_id)
   response.status(200).json(data[0])
 }
