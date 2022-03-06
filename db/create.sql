3c5a8972ecab8d299afde5d4e3a16134ddc08c3b
Loisvanritter <74650343+Loisvanritter@users.noreply.github.com>
2022-03-06 21:30:26 +0000

    laatste tabel extra

--- db/create.sql
+++ db/create.sql
@@ -69,4 +69,8 @@ CREATE TABLE bloemsoorten (
   insert into bloemsoorten (bloem, boeket) values ('lelies', (1, 2, 6));
   insert into bloemsoorten (bloem, boeket) values ('anjers', (3, 4));
   insert into bloemsoorten (bloem, boeket) values ('rozen', (1, 3, 4, 5, 6));
+  insert into bloemsoorten (bloem, boeket) values ('roos', 7);
+  insert into bloemsoorten (bloem, boeket) values ('gerbera', 8);
+  insert into bloemsoorten (bloem, boeket) values ('tulpen',9);
+  insert into bloemsoorten (bloem, boeket) values ('zonnebloem', 10);
 
